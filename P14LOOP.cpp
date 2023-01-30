6d69e83d0177dd9485f7973b0b9da2fd332771a2
Geethika <83590629+geethika1129@users.noreply.github.com>
2023-01-30 21:39:55 +0530

    Update P14LOOP.cpp

--- P14LOOP.cpp
+++ P14LOOP.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main() {
 int n,sum=0;                              // additive identity concept applies here=>(0+1=1)
 cout<<"enter any number: ";
-cin>>n;
+cin>>n; //input the number required
 
 for (int i =1 ; i <=n; i++)
 {
-    sum+=i;
+    sum+=i; //add the induvidual number to variable sum in each iteration
 }
 cout<<"sum= "<<sum;
 
